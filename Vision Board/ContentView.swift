15c326a2cb6ce314ed0a3acbac72476f4e924515
1Dinakar <dinakarvenugopal1117@gmail.com>
2024-06-04 13:04:45 +1000

    Initial Commit

---
+++ Vision Board/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vision Board
+//
+//  Created by Dinakar Venugopal on 4/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
